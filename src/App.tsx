aca3953a8ce50614912fc2897a7b4241b64885a9
Edgar A. Barragán <edgar@MacBook-Air-de-Edgar.local>
2026-07-02 14:47:52 -0500

    Restaurar fondo blanco y eliminar gradientes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,13 @@
 
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { LightGradientBackground } from './components/LightGradientBackground';
 import { LMSHero } from './components/LMSHero';
 import { CourseGrid } from './components/CourseGrid';
 import { FeaturesLMS } from './components/FeaturesLMS';
 
 export default function App() {
   return (
-    <div className="relative w-full overflow-x-hidden bg-slate-50 text-slate-900 font-body antialiased selection:bg-primary/20">
-      <LightGradientBackground />
+    <div className="relative w-full overflow-x-hidden bg-white text-slate-900 font-body antialiased selection:bg-primary/20">
       <Header />
       <main>
         <LMSHero />
